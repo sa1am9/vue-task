5c929984a7f20f2338f4e33220b4673d36450907
Илья Салтыков <ktillsal@gmail.com>
2021-02-15 10:15:54 +0200

    fix bug with local_storage

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -33,24 +33,41 @@
 </template>
 
 <script>
-import tasks from './tasks'
     export default {
         data () {
             return {
-                newTaskName : tasks.data().newTaskName,
-                tasks : tasks.data().tasks
+                tasks :  [
+                    {name : 'create skeleton of todo', complete : true},
+                    {name : 'add ability to add tasks', complete : true},
+                    {name : 'clear task name after clicking "Add"', complete : false},
+                    {name : 'put "Add" button in one line with input', complete : false},
+                    {name : 'add new task by hitting Enter instead of clicking "Add"', complete : false},
+                    {name : 'replace <input> with <ui-checkbox> in tasks list', complete : false},
+                    {name : 'when task is complete cross it out', complete : false},
+                    {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : false},
+                    {name : 'don\'t allow to add empty tasks', complete : false},
+                    {name : 'make list of tasks scrollable, if there\'re are a lot of tasks', complete : false},
+                    {name : 'extract list item into a separate vue.js component', complete : false},
+                    {name : 'persist tasks list in a local storage', complete : false},
+                    {name : 'add animation on task completion', complete : false},
+                ],
+                newTaskName : ''
             }
         },
         computed:{
         },
         mounted () {
             if (localStorage.tasks === undefined){
-                localStorage.tasks = this.tasks
+                localStorage.setItem("tasks", JSON.stringify(this.tasks));
+            }
+            else {
+                console.log(JSON.parse(localStorage.getItem("tasks")))
+                this.tasks = JSON.parse(localStorage.getItem("tasks"));
             }
         },
         watch:{
           tasks(new_tasks){
-              localStorage.tasks = new_tasks
+              localStorage.setItem("tasks", JSON.stringify(this.tasks));
           }
         },
         methods : {
